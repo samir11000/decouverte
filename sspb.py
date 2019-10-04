f9a3ba12ff06e6b579f48d03b70cd7b4e11d8260
samir11000 <ait.ouaret.samir@gmail.com>
2019-10-04 17:23:13 +0200

    merge

--- sspb.py
+++ sspb.py
@@ -1,11 +1,11 @@
-print("Bonjour")
 age = 12
 age_autorise = age +10
 print(age_autorise)
+age_interdit = age - 5
 age_interdit = age / 5
 def verif:
-	print("Ok")
-	val =  "Ok"
-	return val
+       print("Ok")
+       val =  "Ok"
+       return val
 
 print(verif)
