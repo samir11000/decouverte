c6053cb4c0962f23eb786b5b1fc7a614dcaca97c
samir11000 <ait.ouaret.samir@gmail.com>
2019-10-04 16:55:29 +0200

    correction age_interdit et ajout fonction verif

--- sspb.py
+++ sspb.py
@@ -2,4 +2,10 @@ print("Bonjour")
 age = 12
 age_autorise = age +10
 print(age_autorise)
-age_interdit = age - 5
+age_interdit = age / 5
+def verif:
+	print("Ok")
+	val =  "Ok"
+	return val
+
+print(verif)
